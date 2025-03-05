1111899deda3a62d5082a419ba69b8658def099b
et <eileentomy@gmail.com>
2025-03-05 14:09:14 -0500

    Go tutorial to next level

--- Paint by Platformer/Assets/Scripts/Follow_player.cs
+++ Paint by Platformer/Assets/Scripts/Follow_player.cs
@@ -4,10 +4,13 @@ public class Follow_player : MonoBehaviour
 {
     public Transform player;
     public Vector3 vec;
+    public GameObject end;
 
     // Update is called once per frame
     void Update () {
+        
         transform.position = player.transform.position + vec;
+
     }
 
 }
