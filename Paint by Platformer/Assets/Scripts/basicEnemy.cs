ac1836e11331d67e679daf6c7d3dcdd8103027d2
et <eileentomy@gmail.com>
2025-04-04 16:08:36 -0400

    enemy works

--- Paint by Platformer/Assets/Scripts/basicEnemy.cs
+++ Paint by Platformer/Assets/Scripts/basicEnemy.cs
@@ -1,3 +1,4 @@
+using TreeEditor;
 using UnityEngine;
 
 public class basicEnemy : MonoBehaviour
@@ -26,12 +27,15 @@ public class basicEnemy : MonoBehaviour
                 if( transform.position.x > minDist)
                     {
                        GetComponent <Rigidbody2D>().linearVelocity = new Vector2(-movingSpeed,GetComponent<Rigidbody2D>().linearVelocityY);
-                       Debug.Log("moving left " +direction);
+                       //Debug.Log("moving left " +direction);
                        
                     }
                 else
                     {
+                        Vector3 localScale = transform.localScale;
                        direction = 1;
+                       localScale.x *=-1f;
+                       transform.localScale=localScale;
                     }
                 break;
              case 1:
@@ -39,11 +43,14 @@ public class basicEnemy : MonoBehaviour
                 if(transform.position.x < maxDist)
                     {
                         GetComponent <Rigidbody2D>().linearVelocity = new Vector2(movingSpeed,GetComponent<Rigidbody2D>().linearVelocityY);
-                        Debug.Log("moving right");
+                        //Debug.Log("moving right");
                     }
                 else
                     {
+                        Vector3 localScale = transform.localScale;
                         direction = -1;
+                        localScale.x *=-1f;
+                       transform.localScale=localScale;
                     }
                 break;
         }
