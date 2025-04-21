acfa7ed58d04c1921547bf725d8eec8a064813d4
Ariana Lee <jarimlee02@gmail.com>
2025-04-21 19:18:40 -0400

    Revert "Dash, double jump, and star animations added"
    This reverts commit 6604005e624c86dc4d80641d2f44acf7052efc44.

--- Paint by Platformer/Assets/Scripts/GoobPlayerScript.cs
+++ Paint by Platformer/Assets/Scripts/GoobPlayerScript.cs
@@ -154,7 +154,6 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetFloat("Vertical", 1);
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower * 0.6f);
-            animator.SetTrigger("DubJump");
             canDoubleJump = false;
         }
 
